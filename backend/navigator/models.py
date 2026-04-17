9524f8c947c43699f6f43a594fb187e4e78e0a15
nicolecoloma <138119129+nicolecoloma@users.noreply.github.com>
2026-04-17 13:37:26 +0300

    Add Location model with fields for name and coordinates

---
+++ backend/navigator/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
